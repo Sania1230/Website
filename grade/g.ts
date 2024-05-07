fc8533c84ad51d1673fac1edd1cd93b31616d9b7
Sania1230 <164331038+Sania1230@users.noreply.github.com>
2024-05-07 12:44:26 +0500

    Add files via upload

---
+++ grade/g.ts
@@ -0,0 +1,19 @@
+let marks: number=45;
+if(marks >= 90){
+    console.log("A+");
+}
+else if (marks>=80 && marks<=90 ){
+    console.log("A");
+}
+else if (marks>= 70 && marks <= 80){
+    console.log("B")
+}
+else if (marks>= 60 && marks <= 70){
+    console.log("C")
+}
+else if (marks>= 50 && marks <= 60){
+    console.log("D")
+}
+else {
+    console.log("Fail")
+}
